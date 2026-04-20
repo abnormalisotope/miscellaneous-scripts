28fd8a7271121c11b2b65ee632085e021463a395
ly <120387131+abnormalisotope@users.noreply.github.com>
2026-04-20 19:46:11 -0300

    Update vocaloid6_vtbr.py

--- vocaloid6_vtbr.py
+++ vocaloid6_vtbr.py
@@ -1,9 +1,10 @@
 """
 for the record this can decrypt/encrypt any of the models used by vocaloid6
 and yes. theyre just a big .json, in fact vxb has strings like "Load model (.vtb2/.json)" or something similar
-and no. v6 does not use hts...
+and no. v6 does not use hts... ~afaik~
 hts is only used for the auto pitch introduced in v5 that is still available in v6 for std banks
 check for yourself, its on VLC.dll (which im guessing stands for VOCALOID Lua Controller)
+aaaaand if you want to look into parsing these models i believe Func_2c6cd280 in S5API.dll is responsible for it
 """
 import sys
 import json
